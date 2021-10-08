b0d1f0f6898e7badba3189ef944c37a0b3eabb69
Leandro Santos <leandro.santos1986@gmail.com>
2021-10-08 10:48:17 -0300

    Disable Microsoft ClickToRun on this script

--- Scripts/DisableFeaturesServices.ps1
+++ Scripts/DisableFeaturesServices.ps1
@@ -22,6 +22,6 @@ Set-Service -Name "VSS" -StartupType Disabled
 Stop-Service -Force -Name "VSS"
 
 #Desativar Microsoft Office Click to Run:
-Write-Progress -Activity 'Disabling Microsoft Office Click to Run...' -PercentComplete (100/10 * 3)
-Set-Service -Name "ClickToRunSvc" -StartupType Disabled
-Stop-Service -Force -Name "ClickToRunSvc"
+#Write-Progress -Activity 'Disabling Microsoft Office Click to Run...' -PercentComplete (100/10 * 3)
+#Set-Service -Name "ClickToRunSvc" -StartupType Disabled
+#Stop-Service -Force -Name "ClickToRunSvc"
