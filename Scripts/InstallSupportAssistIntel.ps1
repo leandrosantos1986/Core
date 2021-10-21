1c452125d5913e583e8c0a48f25e76c14e9b7d57
Leandro Santos <leandro.santos1986@gmail.com>
2021-10-21 18:16:38 -0300

    Change 'wget' expression to Invoke-WebRequest

--- Scripts/InstallSupportAssistIntel.ps1
+++ Scripts/InstallSupportAssistIntel.ps1
@@ -1,7 +1,7 @@
 ﻿powershell.exe -Command "& {Set-ExecutionPolicy -scope Currentuser -executionPolicy Unrestricted}"
 Write-Progress -Activity 'Installing Intel Support Drivers' -PercentComplete (100/10 * 9)
 New-Item -Type directory -path "C:\Temp\Core\Drivers\Intel\" -Force
-wget 'https://downloadmirror.intel.com/28425/a08/Intel-Driver-and-Support-Assistant-Installer.exe' -OutFile 'C:\Temp\Core\Drivers\Intel\Intel-Driver-and-Support-Assistant-Installer.exe'
+Invoke-WebRequest 'https://downloadmirror.intel.com/28425/a08/Intel-Driver-and-Support-Assistant-Installer.exe' -OutFile 'C:\Temp\Core\Drivers\Intel\Intel-Driver-and-Support-Assistant-Installer.exe'
 & 'C:\Temp\Core\Drivers\Intel\Intel-Driver-and-Support-Assistant-Installer.exe' /q /doNotRequireDRMPrompt -Force
 #Start-Sleep 60
 Start-Process microsoft-edge:https://www.intel.com.br/content/www/br/pt/support/detect.html
