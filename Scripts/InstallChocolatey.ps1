6e70de3749d7eb72e9e5aa2113dabf96308954ad
Leandro Santos <leandro.santos1986@gmail.com>
2021-10-21 16:24:40 -0300

    Fix 'iex' to Invoke-Expression argument

--- Scripts/InstallChocolatey.ps1
+++ Scripts/InstallChocolatey.ps1
@@ -8,4 +8,4 @@ if (Test-Path($ChocolateyProfile)) {
 }
 '@ | Add-Content -Path $profile
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
