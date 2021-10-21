ba068171fa88823918dd617271e7fdc8b704fa28
Leandro Santos <leandro.santos1986@gmail.com>
2021-10-21 18:15:01 -0300

    Change 'wget' expression to Invoke-WebRequest

--- Scripts/InstallSupportAssistDell.ps1
+++ Scripts/InstallSupportAssistDell.ps1
@@ -1,7 +1,7 @@
 ﻿powershell.exe -Command "& {Set-ExecutionPolicy -scope Currentuser -executionPolicy Unrestricted}"
 Write-Progress -Activity 'Installing Dell SupportAssist' -PercentComplete (100/10 * 9)
 New-Item -Type directory -path "C:\Temp\Core\Drivers\Dell\" -Force
-wget 'https://dl.dell.com/serviceability/eSupport/SupportAssistLauncher.exe' -OutFile 'C:\Temp\Core\Drivers\Dell\SupportAssistLauncher.exe'
+Invoke-WebRequest 'https://dl.dell.com/serviceability/eSupport/SupportAssistLauncher.exe' -OutFile 'C:\Temp\Core\Drivers\Dell\SupportAssistLauncher.exe'
 & 'C:\Temp\Core\Drivers\Dell\SupportAssistLauncher.exe' 
 #Start-Sleep 60
 Start-Process microsoft-edge:https://www.dell.com/support/home/pt-br/product-support/servicetag
