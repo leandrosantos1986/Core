30aeeed1113b31ec991e562cd103c321f6aa143d
Leandro Santos <leandro.santos1986@gmail.com>
2021-11-01 16:43:32 -0300

    Reordered line command

--- Scripts/InstallChocolatey.ps1
+++ Scripts/InstallChocolatey.ps1
@@ -1,6 +1,6 @@
 ﻿#inspowershell.exe -Command "& {Set-ExecutionPolicy -scope Currentuser -executionPolicy Unrestricted}"
-powershell.exe -NoLogo -NoProfile -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.6 -Scope CurrentUser -AllowClobber -Repository PSGallery'
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+powershell.exe -NoLogo -NoProfile -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.6 -Scope CurrentUser -AllowClobber -Repository PSGallery'
 
 @'
 # Chocolatey profile
