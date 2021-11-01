d3f0e67d8b85e50867db0585edcb9df19dd5540f
Leandro Santos <leandro.santos1986@gmail.com>
2021-11-01 16:12:54 -0300

    Added line to install Chocolatey first

--- Scripts/InstallCore.bat
+++ Scripts/InstallCore.bat
@@ -1,4 +1,5 @@
 ﻿@ECHO OFF 
 powershell.exe -Command "& {Set-ExecutionPolicy -scope Currentuser -executionPolicy Unrestricted}"
+powershell.exe %~dp0%InstallChocolatey.ps1
 powershell.exe %~dp0%InstallCore.ps1
 PAUSE
