e0b3591137673cbf8e976d6c2e99f8d20b861476
Leandro Santos <leandro.santos1986@gmail.com>
2021-11-01 16:14:17 -0300

    Commented line to exclude Chococolatey Install

--- Scripts/InstallCore.ps1
+++ Scripts/InstallCore.ps1
@@ -53,7 +53,7 @@ Write-Progress -Activity 'Installing Microsoft 365' -PercentComplete (100/10 * 8
 powershell.exe -File C:\Temp\Core\Scripts\InstallM365.ps1
 
 Write-Progress -Activity 'Installing Chocolatey' -PercentComplete (100/10 * 9)
-powershell.exe -File C:\Temp\Core\Scripts\InstallChocolatey.ps1
+#powershell.exe -File C:\Temp\Core\Scripts\InstallChocolatey.ps1
 
 Write-Progress -Activity 'Installing Forticlient VPN' -PercentComplete (100/10 * 9)
 powershell.exe -File C:\Temp\Core\Scripts\InstallVPN.ps1
