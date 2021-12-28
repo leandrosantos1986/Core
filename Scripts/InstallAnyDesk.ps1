75f01fbf79854684a22264160a35eafd1c7e327e
Leandro Santos <leandro.santos1986@gmail.com>
2021-12-28 18:34:47 -0300

    Added TLS older to download client

--- Scripts/InstallAnyDesk.ps1
+++ Scripts/InstallAnyDesk.ps1
@@ -1,4 +1,5 @@
 ﻿Set-ExecutionPolicy Unrestricted -Force
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
 Write-Progress -Activity 'Installing AnyDesk (64-bit)' -PercentComplete (100/10 * 9)
 New-Item -Type Directory -Path "C:\Temp\Core\Tools\Remote\AnyDesk\" -Force
 Invoke-WebRequest 'https://download.anydesk.com/AnyDesk.msi' -OutFile 'C:\Temp\Core\Tools\Remote\AnyDesk\AnyDesk.msi'
