a13f145132128690e08cd9803df031f5fc750275
Leandro Santos <leandro.santos1986@gmail.com>
2022-03-08 12:28:19 -0300

    Removed Install Teams Command

--- Scripts/InstallChocolateyApps.ps1
+++ Scripts/InstallChocolateyApps.ps1
@@ -3,7 +3,7 @@
 choco install 7zip -y --force
 choco install adobereader -y --force
 choco install googlechrome -y --force
-choco install microsoft-teams -y --force
+#choco install microsoft-teams -y --force
 choco install notepadplusplus -y --force
 choco install speedtest -y --force
 choco install wget -y --force
