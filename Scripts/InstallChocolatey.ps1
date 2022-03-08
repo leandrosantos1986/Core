105457b5d682b59b0d3fa2b46f9ca3f2c5a763e2
Leandro Santos <leandro.santos1986@gmail.com>
2022-03-08 11:13:37 -0300

    Added Command to install NuGet v2.8.5.201

--- Scripts/InstallChocolatey.ps1
+++ Scripts/InstallChocolatey.ps1
@@ -1,4 +1,5 @@
-﻿powershell.exe -NoLogo -NoProfile -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.6 -Scope CurrentUser -AllowClobber -Repository PSGallery'
+﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+powershell.exe -NoLogo -NoProfile -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.7 -Scope CurrentUser -AllowClobber -Repository PSGallery'
 
 @'
 # Chocolatey profile
