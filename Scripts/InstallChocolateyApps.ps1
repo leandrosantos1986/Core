81d1593d9b840329fd8c7792916d5680e0d58861
Leandro Santos <leandro.santos1986@gmail.com>
2022-03-09 02:08:26 -0300

    Command to remove broken Microsoft Teams

--- Scripts/InstallChocolateyApps.ps1
+++ Scripts/InstallChocolateyApps.ps1
@@ -1,9 +1,10 @@
 ﻿Set-ExecutionPolicy Unrestricted -Force
+Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq "Teams Machine-Wide Installer"} | foreach-object -process {$_.Uninstall()}
 
 choco install 7zip -y --force
 choco install adobereader -y --force
 choco install googlechrome -y --force
-#choco install microsoft-teams -y --force
+choco install microsoft-teams -y --force
 choco install notepadplusplus -y --force
 choco install speedtest -y --force
 choco install wget -y --force
