eb24e6521ed8344c74e81e1705dbfc46344c8a80
Leandro Santos <leandro.santos1986@gmail.com>
2022-12-08 14:00:09 -0300

    Update Script

--- Scripts/CheckAndInstallWU.ps1
+++ Scripts/CheckAndInstallWU.ps1
@@ -1,5 +1,7 @@
 ﻿Set-ExecutionPolicy Unrestricted -Force
+Install-Module PowerShellGet -AllowClobber -Force
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module PSWindowsUpdate -Force 
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$False
 Get-WindowsUpdate
