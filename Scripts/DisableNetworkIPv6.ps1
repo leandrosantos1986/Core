a4baeac3a51bcf299b8b67c0a918dbc5fc654df1
Leandro Santos <leandro.santos1986@gmail.com>
2022-12-15 18:02:27 -0300

    Added command to change network to private mode

--- Scripts/DisableNetworkIPv6.ps1
+++ Scripts/DisableNetworkIPv6.ps1
@@ -1,3 +1,3 @@
 Get-NetAdapter | ForEach-Object { Disable-NetAdapterBinding -InterfaceAlias $_.Name -ComponentID ms_tcpip6 }
-
+Set-NetConnectionProfile -InterfaceAlias * -NetworkCategory Private
 Get-NetConnectionProfile
