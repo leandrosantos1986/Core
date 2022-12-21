e49d6d9b1aacf1b34ce0405cb3065f36ce00c44d
Leandro Santos <leandro.santos1986@gmail.com>
2022-12-21 14:24:51 -0300

    Verify

--- Scripts/InstallChocolatey.ps1
+++ Scripts/InstallChocolatey.ps1
@@ -1,16 +1,3 @@
-﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-powershell.exe -NoLogo -NoProfile -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Install-Module -Name PackageManagement -Force -MinimumVersion 1.4.7 -Scope CurrentUser -AllowClobber -Repository PSGallery'
-
-@'
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-'@ | Add-Content -Path $profile
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-#Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 refreshenv
 Update-SessionEnvironment
